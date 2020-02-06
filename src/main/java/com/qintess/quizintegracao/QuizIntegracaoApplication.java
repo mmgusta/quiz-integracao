96eb18a0dba2dd3a019a22d715b6cf5f08dd3395
mmgusta <gustavo.marinelli@gmail.com>
2020-02-06 18:12:29 -0200

    first commit

---
+++ src/main/java/com/qintess/quizintegracao/QuizIntegracaoApplication.java
@@ -0,0 +1,13 @@
+package com.qintess.quizintegracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizIntegracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizIntegracaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qintess/quizintegracao/QuizIntegracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qintess.quizintegracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizIntegracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
